155a82bb98b64ac7e7043fa592d78238259a9579
stanley chukwu <stanleychukwu17@yahoo.com>
2022-11-01 14:52:29 +0100

    done with the project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import './app.scss';
-// import { motion } from 'framer-motion';
+import { motion, Variant } from 'framer-motion';
 
 // import the assets we will be using
 import img1 from '../../assets/img1.jpg'
@@ -10,16 +10,31 @@ import img5 from '../../assets/img5.png'
 import img6 from '../../assets/img6.png'
 const images = {img1, img6, img3, img4, img5, img2}
 
-const App = () => {
+// motion variant
+const cardVariant: {initial?: Variant, animate?: Variant} = {
+    initial: (custom) => ({
+        y: -60, opacity:0,
+        x: (custom % 2 === 0) ? 50 : -50,
+    }),
+    animate: (custom) => ({
+        y:0, opacity:1, x:0,
+        transition: {
+            duration: .5,
+            delay: custom * 0.2,
+            ease: "easeOut"
+        }
+    })
+};
 
+const App = () => {
     return (
         <div className="AppMain">
             <div className="imgGeneral">
-                {Object.values(images).map(item => {
+                {Object.values(images).map((item, index) => {
                     return (
-                        <div className="ImgCvr" key={item}>
+                        <motion.div className="ImgCvr" key={item} variants={cardVariant} initial="initial" animate="animate" custom={index}>
                             <img src={item} alt="" />
-                        </div>
+                        </motion.div>
                     )
                 })}
             </div>
